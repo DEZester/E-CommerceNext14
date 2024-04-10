3883ca2b8cf6c0ae59407cf5da4566d394217922
DEZester <aromudez@gmail.com>
2024-04-10 21:57:38 +0300

    Create Logo

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <div>Navbar</div>
+    <header className="mb-8 border-b">
+      <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
+        <Link href={"/"}>
+          <h1 className='text-4xl font-bold'>Flashy <span className="text-primary">Shop</span> </h1>
+        </Link>
+      </div>
+    </header>
   );
 }
 

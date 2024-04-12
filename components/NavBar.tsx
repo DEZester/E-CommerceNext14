cdbb2a8c8d2eb61c3a2c2d3c4305b3de7c991c37
DEZester <aromudez@gmail.com>
2024-04-12 17:04:05 +0300

    Update NavBar component for responsiveness
    The NavBar component has been updated to be more visually appealing on smaller screens. The logo size has been reduced for better fit and a "Cart" label has been added to the shopping bag button for clarity.

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -31,7 +31,7 @@ export default function NavBar() : React.JSX.Element {
     <header className="mb-8 border-b">
       <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
         <Link href={"/"}>
-          <h1 className='text-4xl font-bold'>Flashy <span className="text-primary">Shop</span> </h1>
+          <h1 className='text-2xl md:text-4xl font-bold'>Flashy<span className="text-primary">Shop</span> </h1>
         </Link>
         <nav className='hidden gap-12 lg:flex 2xl:ml-16'>
           {links.map((link, idx) => <div key={idx}>
@@ -45,6 +45,7 @@ export default function NavBar() : React.JSX.Element {
         <div className='flex divide-x border-r sm:border-l'>
           <Button variant={"outline"} className='flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none'>
             <ShoppingBag />
+            <span className='hidden text-xs font-semibold text-gray sm:block'>Cart</span>
           </Button>
         </div>
       </div>

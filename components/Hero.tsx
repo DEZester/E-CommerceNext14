ec05c2723ed101b0224a86dba00c98afbc979128
DEZester <aromudez@gmail.com>
2024-04-12 21:35:47 +0300

    Refactor Hero component layout
    The image placement in the Hero component has been adjusted to improve its overall layout. Removed unnecessary spacing and aligned the two image sections closer to each other, providing a cleaner site design.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,27 +19,27 @@ export default async function Hero() {
         <p className='max-w-md leading-relaxed text-gray-500 xl:text-lg'>
           We are the leading online fashion store in the world. We provide the best online fashion products for you.
         </p>
-
-        <div className="mb-12 flex w-full md:mb-16 lg:w-2/3">
-          <div
-            className='relative left-12 top-12 z-10 -ml-12 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:left-16 md:top-16 lg:ml-0'>
-            <Image
-              src={urlFor(data.image1).url()}
-              alt='Photo1'
-              className='h-full w-full object-cover object-center'
-              width={500}
-              height={500}
-            />
-          </div>
-          <div className='overflow-hidden rounded-lg bg-gray-100 shadow-lg'>
-            <Image
-              src={urlFor(data.image2).url()}
-              alt='Photo2'
-              className='h-full w-full object-cover object-center'
-              width={500}
-              height={500}
-            />
-          </div>
+        
+      </div>
+      <div className="mb-12 flex w-full md:mb-16 lg:w-2/3">
+        <div
+          className='relative left-12 top-12 z-10 -ml-12 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:left-16 md:top-16 lg:ml-0'>
+          <Image
+            src={urlFor(data.image1).url()}
+            alt='Photo1'
+            className='h-full w-full object-cover object-center'
+            width={500}
+            height={500}
+          />
+        </div>
+        <div className='overflow-hidden rounded-lg bg-gray-100 shadow-lg'>
+          <Image
+            src={urlFor(data.image2).url()}
+            alt='Photo2'
+            className='h-full w-full object-cover object-center'
+            width={500}
+            height={500}
+          />
         </div>
       </div>
     </div>

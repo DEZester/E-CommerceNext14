9ae4f0efab4362327e12262a62d97ca0ad8208c6
DEZester <aromudez@gmail.com>
2024-04-15 21:02:56 +0300

    Add category links to Hero component
    Category links for Men, Women, and Teens have been added to the Hero component to improve site navigation. Import statement for "next/link" was also added to enable this feature, enhancements improving user experience by providing direct links to specific product categories.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import {client, urlFor} from "@/app/lib/sanity";
+import Link from "next/link";
 
 async function getData() {
   const query = "*[_type == 'heroImage'][0]"
@@ -9,6 +10,7 @@ async function getData() {
 }
 
 export default async function Hero() {
+
   const data = await getData()
   return <section className='mx-auto max-w-2xl px-4 sm:pb-6 lg:max-w-7xl lg:px-8'>
     <div className="mb-8 flex flex-wrap justify-between mb:mb-16">
@@ -19,7 +21,7 @@ export default async function Hero() {
         <p className='max-w-md leading-relaxed text-gray-500 xl:text-lg'>
           We are the leading online fashion store in the world. We provide the best online fashion products for you.
         </p>
-        
+
       </div>
       <div className="mb-12 flex w-full md:mb-16 lg:w-2/3">
         <div
@@ -41,6 +43,22 @@ export default async function Hero() {
             height={500}
           />
         </div>
+      </div>
+    </div>
+    <div className='flex flex-col items-center justify-between gap-8 md:flex-row'>
+      <div className='flex h-12 w-64 divide-x overflow-hidden rounded-lg border'>
+        <Link href="/Men"
+              className='flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200'>
+          Men
+        </Link>
+        <Link href="/Women"
+              className='flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200'>
+          Women
+        </Link>
+        <Link href="/Teens"
+              className='flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200'>
+          Teens
+        </Link>
       </div>
     </div>
   </section>

485c5868e632fc62c550fed7cd3233c890812f6c
DEZester <aromudez@gmail.com>
2024-04-16 12:35:15 +0300

    Add ImageGallery component and integrate with product page
    Implemented a new ImageGallery component that gets and displays images from the sanity.io dataset. Additionally, the ImageGallery has been integrated into the product page to allow for display of product images.

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {client} from "@/app/lib/sanity";
 import {fullProduct} from "@/app/interface";
+import ImageGallery from "@/components/ImageGallery";
 
 async function getData(slug: string) {
   const query = `*[_type == 'product' && slug.current == "${slug}"] [0] {_id, price, name, "slug": slug.current, images}`
@@ -11,6 +12,12 @@ async function getData(slug: string) {
 export default async function ProductPage({params}: { params: { slug: string } }) {
   const data: fullProduct = await getData(params.slug)
   return (
-    <div>Hello from ProductPage</div>
+    <div className='bg-white'>
+      <div className='mx-auto max-w-screen-xl px-4 md:px-8'>
+        <div className='grid gap-8 md:grid-cols-2'>
+          <ImageGallery images={data.images}/>
+        </div>
+      </div>
+    </div>
   );
 }
---
+++ components/ImageGallery.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import Image from "next/image";
+import {urlFor} from "@/app/lib/sanity";
+import {useState} from "react";
+
+interface iAppProps {
+  images: any
+}
+
+export default function ImageGallery({images}: iAppProps) {
+  const [bigImage, setBigImage] = useState(images[0])
+
+  return <div className='grid gap-4 lg:grid-cols-5'>
+    <div className='order-last flex gap-4 lg:order-none lg:flex-col'>
+      {images.map((image: any, idx: any) => <div key={idx} className='overflow-hidden rounded-lg bg-gray-100'>
+        <Image
+          src={urlFor(image).url()}
+          alt='Photo1'
+          className='h-full w-full object-cover object-center cursor-pointer'
+          width={200}
+          height={200}
+        />
+      </div>)}
+    </div>
+    <div className='relative overflow-hidden rounded-lg bg-gray-100 lg:col-span-4'>
+      <Image
+        src={urlFor(bigImage).url()}
+        alt='Photo'
+        className='h-full w-full object-cover object-center cursor-pointer'
+        width={500}
+        height={500}
+        onClick={() => setBigImage(images[0])}
+      />
+    </div>
+
+  </div>
+}
